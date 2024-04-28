2b84a33643022286f42c982f9ca121d2bf5b2429
Omar Talat <dr.omartalat@gmail.com>
2024-04-27 22:45:19 -0700

    0x0A-configuration_management enhanced 4

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
-#!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
 
-package { 'Flask':
+package { 'Werkzeug':
+  pkgname    => 'Werkzeug',
+  ensure     => '2.0.0',
+  virtualenv => '/path/to/virtualenv',
+}
+
+package { 'flask':
+  name     => 'flask',
   ensure   => '2.1.0',
   provider => 'pip3',
 }
