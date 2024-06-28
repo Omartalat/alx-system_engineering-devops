2aaa168aa8fdecf1362c5a0a66f94481ff84c013
Omar Talat <dr.omartalat@gmail.com>
2024-06-28 03:55:58 -0700

    1-install_a_package.pp done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
-#  install flask from pip3.
-
-package { 'pip3':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
-  require => Package['pip3'],
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
