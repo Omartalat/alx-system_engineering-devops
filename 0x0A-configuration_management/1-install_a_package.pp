922c12957528cc2d260bd5060015fefbbfbecaf0
dr.omartalat <dr.omartalat@gmail.com>
2024-06-28 13:11:08 +0300

    0x0A-configuration_management first checker trial

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+#  install flask from pip3.
+
+package { 'pip3':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
+  require => Package['pip3'],
+}
