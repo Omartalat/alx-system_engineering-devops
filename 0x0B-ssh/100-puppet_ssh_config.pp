e959cdc1e74602f7066d11f6ec2d06a58e191ca8
Omartalat <dr.omartalat@gmail.com>
2024-06-28 23:19:35 -0700

    puppet-lint fixed last one

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 file_line{'use the private key ~/.ssh/school':
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
 }
 
 file_line{'refuse to authenticate using a password':
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no'
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no'
 }
