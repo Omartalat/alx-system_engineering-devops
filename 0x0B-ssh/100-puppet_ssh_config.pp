4c10ab4f4025a265eefac5e8ac76fae257f6c8e4
Omartalat <dr.omartalat@gmail.com>
2024-06-30 01:08:53 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,9 @@
-# using puppet to make changes to our configuration file.
+#!/usr/bin/env bash
+# Automating my Tasks using Puppet
 
-file { '/etc/ssh/ssh_cofig':
+file { '/etc/ssh/ssh_config':
   ensure  => present,
-  content => "
+content => "
     # SSH client configuration
     Host *
       IdentityFile ~/.ssh/school
